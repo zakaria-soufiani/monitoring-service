c3db9a07be8de5ecf793df14356f18ed5dd94bbb
Zakaria Soufiani <mz.soufiani@gmail.com>
2020-10-06 20:09:23 +1100

    listing security groups for each instance

--- src/handler.js
+++ src/handler.js
@@ -4,11 +4,18 @@ const region = process.env.AWS_REGION
 AWS.config.update({ region: region});
 const ec2 = new AWS.EC2({ apiVersion: "2016-11-15" });
 
+const securityGroupsList = [];
+
 module.exports.listEC2SecurityGroups = async (event) => {
   let ec2List = await getEc2List();
+  ec2List.Reservations.forEach(reservation => {
+    reservation.Instances.forEach(instance => {
+      securityGroupsList.push(...instance.SecurityGroups)
+    })
+  });
   return {
     statusCode: 200,
-    body: JSON.stringify(ec2List),
+    body: JSON.stringify(securityGroupsList),
   };
 };
 
