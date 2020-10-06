e9b2f4e20ccdcfea00972585c20938735e8ff545
Zakaria Soufiani <mz.soufiani@gmail.com>
2020-10-06 21:57:34 +1100

    make response json 1.0 format

--- src/handler.js
+++ src/handler.js
@@ -1,23 +1,41 @@
 'use strict';
+var _ = require('lodash');
 const AWS = require("aws-sdk");
-const region = process.env.AWS_REGION
+const region = process.env.AWS_REGION;
 AWS.config.update({ region: region});
 const ec2 = new AWS.EC2({ apiVersion: "2016-11-15" });
 
-const securityGroupsList = [];
+var securityGroupsList = [];
 
 module.exports.listEC2SecurityGroups = async (event) => {
   let ec2List = await getEc2List();
   ec2List.Reservations.forEach(reservation => {
     reservation.Instances.forEach(instance => {
-      securityGroupsList.push(...instance.SecurityGroups)
+      securityGroupsList = _.unionWith(securityGroupsList, instance.SecurityGroups, _.isEqual)
     })
   });
   return {
     statusCode: 200,
-    body: JSON.stringify(securityGroupsList),
+    body: JSON.stringify(formatResponse(securityGroupsList)),
   };
 };
+
+const formatResponse = (groups) => {
+  let response = {
+    data: []
+  }
+  groups.forEach(group => {
+    let formattedGroup = {
+      type: 'securityGroup',
+      id: group.GroupId,
+      attributes: {
+        name: group.GroupName
+      }
+    }
+    response.data.push(formattedGroup)
+  })
+  return response
+}
 
 const getEc2List = async () => {
   var params = {
