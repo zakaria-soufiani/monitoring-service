de17ade8861404dc24a5da745a998e4a31c72eba
Zakaria Soufiani <mz.soufiani@gmail.com>
2020-10-06 22:38:04 +1100

    added linter

--- src/handler.js
+++ src/handler.js
@@ -1,19 +1,23 @@
-'use strict';
-var _ = require('lodash');
-const helper = require("./helper")
+"use strict";
+var _ = require("lodash");
+const helper = require("./helper");
 
-module.exports.listEC2SecurityGroups = async (event) => {
+module.exports.listEC2SecurityGroups = async () => {
   var securityGroupsList = [];
   let ec2List = await helper.getEc2List();
-  ec2List.Reservations.forEach(reservation => {
-    reservation.Instances.forEach(instance => {
-      securityGroupsList = _.unionWith(securityGroupsList, instance.SecurityGroups, _.isEqual)
-    })
+  ec2List.Reservations.forEach((reservation) => {
+    reservation.Instances.forEach((instance) => {
+      securityGroupsList = _.unionWith(
+        securityGroupsList,
+        instance.SecurityGroups,
+        _.isEqual
+      );
+    });
   });
   return {
     statusCode: 200,
     headers: {
-      "Content-Type": "application/vnd.api+json"
+      "Content-Type": "application/vnd.api+json",
     },
     body: JSON.stringify(helper.formatResponse(securityGroupsList)),
   };
--- src/helper.js
+++ src/helper.js
@@ -1,36 +1,34 @@
- 
- 'use strict'
- const AWS = require("aws-sdk");
- const region = process.env.AWS_REGION;
- AWS.config.update({ region: region});
- const ec2 = new AWS.EC2({ apiVersion: "2016-11-15" });
+"use strict";
+const AWS = require("aws-sdk");
+const region = process.env.AWS_REGION;
+AWS.config.update({ region: region });
+const ec2 = new AWS.EC2({ apiVersion: "2016-11-15" });
 
- module.exports.getEc2List = async () => {
-    var params = {
-      DryRun: false,
-    };
-    try {
-      let data = await ec2.describeInstances(params).promise();
-      console.log("EC2 Instances", data);
-      return data;
-    } catch (e) {
-      throw new Error(`Could not describeInstances EC2 instances ${e.message}`);
-    }
+module.exports.getEc2List = async () => {
+  var params = {
+    DryRun: false,
+  };
+  try {
+    let data = await ec2.describeInstances(params).promise();
+    return data;
+  } catch (e) {
+    throw new Error(`Could not describeInstances EC2 instances ${e.message}`);
   }
+};
 
 module.exports.formatResponse = (groups) => {
-    let response = {
-      data: []
-    }
-    groups.forEach(group => {
-      let formattedGroup = {
-        type: 'securityGroup',
-        id: group.GroupId,
-        attributes: {
-          name: group.GroupName
-        }
-      }
-      response.data.push(formattedGroup)
-    })
-    return response
-  }
+  let response = {
+    data: [],
+  };
+  groups.forEach((group) => {
+    let formattedGroup = {
+      type: "securityGroup",
+      id: group.GroupId,
+      attributes: {
+        name: group.GroupName,
+      },
+    };
+    response.data.push(formattedGroup);
+  });
+  return response;
+};
