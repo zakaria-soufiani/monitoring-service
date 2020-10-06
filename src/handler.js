1de179f9e12edab325a8d3e1eed494bd9315a697
Zakaria Soufiani <mz.soufiani@gmail.com>
2020-10-06 22:12:28 +1100

    moved functions to helper file

--- src/handler.js
+++ src/handler.js
@@ -1,14 +1,10 @@
 'use strict';
 var _ = require('lodash');
-const AWS = require("aws-sdk");
-const region = process.env.AWS_REGION;
-AWS.config.update({ region: region});
-const ec2 = new AWS.EC2({ apiVersion: "2016-11-15" });
-
-var securityGroupsList = [];
+const helper = require("./helper")
 
 module.exports.listEC2SecurityGroups = async (event) => {
-  let ec2List = await getEc2List();
+  var securityGroupsList = [];
+  let ec2List = await helper.getEc2List();
   ec2List.Reservations.forEach(reservation => {
     reservation.Instances.forEach(instance => {
       securityGroupsList = _.unionWith(securityGroupsList, instance.SecurityGroups, _.isEqual)
@@ -16,36 +12,6 @@ module.exports.listEC2SecurityGroups = async (event) => {
   });
   return {
     statusCode: 200,
-    body: JSON.stringify(formatResponse(securityGroupsList)),
+    body: JSON.stringify(helper.formatResponse(securityGroupsList)),
   };
 };
-
-const formatResponse = (groups) => {
-  let response = {
-    data: []
-  }
-  groups.forEach(group => {
-    let formattedGroup = {
-      type: 'securityGroup',
-      id: group.GroupId,
-      attributes: {
-        name: group.GroupName
-      }
-    }
-    response.data.push(formattedGroup)
-  })
-  return response
-}
-
-const getEc2List = async () => {
-  var params = {
-    DryRun: false,
-  };
-  try {
-    let data = await ec2.describeInstances(params).promise();
-    console.log("EC2 Instances", data);
-    return data;
-  } catch (e) {
-    throw new Error(`Could not describeInstances EC2 instances ${e.message}`);
-  }
-}
